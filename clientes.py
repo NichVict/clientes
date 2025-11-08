59f7a5976b389bcb85906ba8092d998c03b13da8
Vanderlei Alexandre Gasparini <138238431+NichVict@users.noreply.github.com>
2025-11-08 17:59:27 +0000

    Update clientes.py

--- clientes.py
+++ clientes.py
@@ -685,9 +685,9 @@ st.markdown("<br><br>", unsafe_allow_html=True)
 
 st.markdown("<div style='height:1px;background:linear-gradient(90deg,transparent,rgba(0,255,180,0.35),transparent);'></div>", unsafe_allow_html=True)
 
-st.markdown("<br>", unsafe_allow_html=True)
-st.subheader("🧑‍🤝‍🧑 Clientes Cadastrados")
 st.markdown("<br><br>", unsafe_allow_html=True)
+st.subheader("🧑‍🤝‍🧑 Clientes Cadastrados")
+st.markdown("<br>", unsafe_allow_html=True)
 
 # 1️⃣ Buscar dados
 try:
